f841c2247749c289e73faa95fe1ec60abf90b934
SiansandaTamara <tamarasiansanda18@gmail.com>
2022-08-10 23:10:45 +0200

    add decorator file

---
+++ decorator.rb
@@ -0,0 +1,28 @@
+require_relative './nameable'
+
+class Decorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    if @nameable.correct_name.size >= 11
+      @nameable.correct_name.slice(0, 10)
+    else
+      @nameable.correct_name
+    end
+  end
+end
