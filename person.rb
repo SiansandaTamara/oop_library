013d94316076364f25b9820650d345f79652edf1
SiansandaTamara <tamarasiansanda18@gmail.com>
2022-08-11 17:31:38 +0200

    add association to student ,add rental extension to person

--- person.rb
+++ person.rb
@@ -1,20 +1,33 @@
-class Person
-  attr_reader :id
+require_relative './nameable'
+require_relative './decorator'
+
+class Person < Nameable
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @age = age
     @name = name
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def of_age?
     @age >= 18
   end
 
+  def correct_name
+    @name
+  end
+
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def add_rental(date, person)
+    Rental.new(date, person, self)
   end
 
   private :of_age?
