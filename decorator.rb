0a47811dd1778a3d893692a09f53853745418054
SiansandaTamara <tamarasiansanda18@gmail.com>
2022-08-16 13:58:02 +0200

    refactor decorator.rb, rental.rb

--- decorator.rb
+++ decorator.rb
@@ -10,19 +10,3 @@ class Decorator < Nameable
     @nameable.correct_name
   end
 end
-
-class CapitalizeDecorator < Decorator
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
-
-class TrimmerDecorator < Decorator
-  def correct_name
-    if @nameable.correct_name.size >= 11
-      @nameable.correct_name.slice(0, 10)
-    else
-      @nameable.correct_name
-    end
-  end
-end
