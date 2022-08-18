46eb4261479d10083ec8fa418d86acd474b56d8a
Ezeoke Stanley <ezeokestanley.ifeanyi@gmail.com>
2022-08-18 15:46:29 +0100

    Add corrections from TSE in rental file

--- rental.rb
+++ rental.rb
@@ -5,11 +5,10 @@ class Rental
 
   def initialize(date, book, person)
     @date = date
-
-    @person = person
-    person.rentals << self
-
     @book = book
     book.rentals << self
+    @person = person
+    person.rentals << self
   end
 end
+
