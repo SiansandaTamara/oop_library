0895519c357efd7274c84206647ae7b59c10c997
Ezeoke Stanley <ezeokestanley.ifeanyi@gmail.com>
2022-08-18 18:43:03 +0100

    Add unit test for student

---
+++ spec/student_spec.rb
@@ -0,0 +1,15 @@
+require './student'
+
+describe Student do
+  before :each do
+    @student = Student.new(1, 26, 'TEST', 'TEST')
+  end
+
+  it 'instance of student' do
+    expect(@student.name).to eq 'TEST'
+  end
+
+  it 'if age is correct' do
+    expect(@student.age).to eq 26
+  end
+end
